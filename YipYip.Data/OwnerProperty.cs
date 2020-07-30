50793fc258dfa93e33799491bc0f3e9a78ae3d41
AThooker <austintrent@icloud.com>
2020-07-30 13:58:31 -0400

    owner properties

--- YipYip.Data/OwnerProperty.cs
+++ YipYip.Data/OwnerProperty.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace YipYip.Data
 {
     public class OwnerProperty
     {
+        [Key]
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public int PopertyId { get; set; }
     }
 }
