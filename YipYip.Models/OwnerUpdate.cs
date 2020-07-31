5a61c784c781a9e93b5edccbfac1c5e8ee710454
Lisa Jeffers <lisajeffers0106@gmail.com>
2020-07-31 10:17:59 -0400

    Save before Data Base Overhaul

---
+++ YipYip.Models/OwnerUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YipYip.Models
+{
+    public class OwnerUpdate
+    {
+        [Required]
+        public int OwnerId { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string OwnerName { get; set; }
+        [Required]
+        [Display(Name = "Phone")]
+        public int Phone { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        public int Rating { get; set; }
+    }
+}
---
+++ YipYip/Controllers/OwnerController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YipYip.Models;
+using YipYip.Services;
+
+namespace YipYip.Controllers
+{
+    public class OwnerController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            OwnerService ownerService = CreateOwnerService();
+            var people = ownerService.GetOwners();
+            return Ok(people);
+        }
+        public IHttpActionResult Post(OwnerCreate owner)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateOwnerService();
+
+            if (!service.CreateOwner(owner))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Get(int id)
+        {
+            OwnerService ownerService = CreateOwnerService();
+            var note = ownerService.GetOwnerById(id);
+            return Ok(note);
+        }
+        public IHttpActionResult Put(OwnerUpdate owner)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateOwnerService();
+
+            if (!service.UpdateOwner(owner))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateOwnerService();
+
+            if (!service.OwnerDelete(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private OwnerService CreateOwnerService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var ownerService = new OwnerService(userId);
+            return ownerService;
+        }
+    }
+}
