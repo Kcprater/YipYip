721bba2491d70a4a9e5b0fe3f4468a1c2cd18677
kalebemery <kalebemery1@gmail.com>
2020-08-03 15:29:48 -0700

    Monday changes 2

--- YipYip/Controllers/OwnerController.cs
+++ YipYip/Controllers/OwnerController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNet.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,57 +10,6 @@ using YipYip.Services;
 namespace YipYip.Controllers
 {
     public class OwnerController : ApiController
-    {
-        public IHttpActionResult Get()
-        {
-            ProfileService ownerService = CreateOwnerService();
-            var people = ownerService.GetOwners();
-            return Ok(people);
-        }
-        public IHttpActionResult Post(ProfileCreate owner)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+    { }
 
-            var service = CreateOwnerService();
-
-            if (!service.CreateOwner(owner))
-                return InternalServerError();
-
-            return Ok();
-        }
-        public IHttpActionResult Get(int id)
-        {
-            ProfileService ownerService = CreateOwnerService();
-            var note = ownerService.GetOwnerById(id);
-            return Ok(note);
-        }
-        public IHttpActionResult Put(ProfileUpdate owner)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var service = CreateOwnerService();
-
-            if (!service.UpdateOwner(owner))
-                return InternalServerError();
-
-            return Ok();
-        }
-        public IHttpActionResult Delete(int id)
-        {
-            var service = CreateOwnerService();
-
-            if (!service.OwnerDelete(id))
-                return InternalServerError();
-
-            return Ok();
-        }
-        private ProfileService CreateOwnerService()
-        {
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var ownerService = new ProfileService(userId);
-            return ownerService;
-        }
-    }
 }
---
+++ YipYip/Controllers/ProfileController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YipYip.Models;
+using YipYip.Services;
+
+namespace YipYip.Controllers
+{
+    public class ProfileController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            ProfileService ownerService = CreateOwnerService();
+            var people = ownerService.GetOwners();
+            return Ok(people);
+        }
+        public IHttpActionResult Post(ProfileCreate owner)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateOwnerService();
+
+            if (!service.CreateOwner(owner))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Get(int id)
+        {
+            ProfileService ownerService = CreateOwnerService();
+            var note = ownerService.GetOwnerById(id);
+            return Ok(note);
+        }
+        public IHttpActionResult Put(ProfileUpdate owner)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateOwnerService();
+
+            if (!service.UpdateOwner(owner))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateOwnerService();
+
+            if (!service.OwnerDelete(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private ProfileService CreateOwnerService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var ownerService = new ProfileService(userId);
+            return ownerService;
+        }
+    }
+}
